d6df9f49272b6b0cec9831ce31aea6d197e48532
julius01a <66196006+julius01a@users.noreply.github.com>
2020-10-27 19:14:50 +1100

    maps for beers

---
+++ work/rouhong/basic beer/config.js
@@ -0,0 +1,2 @@
+// API key
+var API_KEY = "pk.eyJ1IjoianVsaXVzMDEtYSIsImEiOiJja2V0bWV0aWMxZzduMnJtcjVucW5vaXZpIn0.zP7LFCGDkgXEGDJFrTKh8A";
---
+++ work/rouhong/basic beer/logic.js
@@ -0,0 +1,42 @@
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var beerIcon = L.icon ({
+  iconUrl: 'http://feedus.media/wp/wp-content/uploads/leaflet-maps-marker-icons/beer_pin-2.png',
+  iconSize: [75, 75]
+})
+var myMap = L.map("map").setView([39.82, -98.57], 4);
+var myFeatureGroup = L.featureGroup().addTo(myMap);
+var marker, test;
+
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+  }).addTo(myMap);
+
+function getBeer() {
+  myFeatureGroup.clearLayers();
+
+  for (var i = 0; i < 5; i += 1) {
+    test = "test " + i;
+    marker = L.marker(getRandomLatLng(),{icon: beerIcon}).addTo(myFeatureGroup).bindPopup("Marker " + test);
+    marker.test = test;
+  }
+
+  function getRandomLatLng() {
+    return [
+      39.82 + 20 * Math.random() - 8,
+      -98.57 + 20 * Math.random() - 8
+    ]
+  }  
+
+
+}
+
