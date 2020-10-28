836b7d14a287ccddd0d604ea82a29ee2f9b99180
Pooja <pooja.isabelle@gmail.com>
2020-10-28 13:08:43 +1100

    Added map files and preference button

---
+++ work/pooja/static/prediction_page/js/logic.js
@@ -0,0 +1,46 @@
+var API_KEY = "pk.eyJ1IjoicG1hbGxhcmQiLCJhIjoiY2tnc3B5eWc1MDI3NzJ4cHQ5anIwbnpoeCJ9.2LB0cYAMj8pOZ-QgyecGxA";
+
+
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var beerIcon = L.icon ({
+  iconUrl: 'http://feedus.media/wp/wp-content/uploads/leaflet-maps-marker-icons/beer_pin-2.png',
+  iconSize: [75, 75]
+})
+var myMap = L.map("map").setView([45, -5], 2);
+var myFeatureBeers = L.featureGroup().addTo(myMap);
+var marker, test;
+
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+  }).addTo(myMap);
+
+function getBeer() {
+  myFeatureBeers.clearLayers();
+
+  for (var i = 0; i < 5; i += 1) {
+    test = "test " + i;
+    marker = L.marker(getRandomLatLng(),{icon: beerIcon}).addTo(myFeatureBeers).bindPopup("Marker " + test);
+    marker.test = test;
+  }
+
+  function getRandomLatLng() {
+    return [
+      45 + 60 * Math.random() - 25,
+      -30 + 100 * Math.random() - 50
+    ]
+  }  
+
+  myMap.fitBounds(myFeatureBeers.getBounds());
+
+}
+
